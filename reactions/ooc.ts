42458618f8e4bb08007d5a36a9cd1d3fc43bd499
ShyMike <122023566+ImShyMike@users.noreply.github.com>
2026-07-10 18:57:04 +0100

    fix: don't send the same ooc multiple times

--- reactions/ooc.ts
+++ reactions/ooc.ts
@@ -1,11 +1,16 @@
 import type { ReactionTrigger } from "../types";
 
+let oocCache: Set<string> = new Set();
+
 export default {
     name: "i-would-ooc-this-but-i-cant",
     any: async (msg, ctx) => {
-        await ctx.client.chatPostMessage({
-            channel: ctx.userChannelId,
-            text: `Waiter, waiter! One more <https://${ctx.workspace}.slack.com/archives/${msg.channel}/p${msg.ts.replace(".", "")}|OOC> please!`,
-        });
+        if (!oocCache.has(`${msg.channel}-${msg.ts}`)) {
+            await ctx.client.chatPostMessage({
+                channel: ctx.userChannelId,
+                text: `Waiter, waiter! One more <https://${ctx.workspace}.slack.com/archives/${msg.channel}/p${msg.ts.replace(".", "")}|OOC> please!`,
+            });
+            oocCache.add(`${msg.channel}-${msg.ts}`);
+        }
     },
 } satisfies ReactionTrigger;
